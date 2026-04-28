7a2e248310785f9ac292c7824c08575a8e0dbf3a
dinea12303-ux <dinea12303@gmail.com>
2026-04-28 23:43:04 +0000

    Remove background gradient overlay

--- assets/app.js
+++ assets/app.js
@@ -73,8 +73,7 @@ hero.style.backgroundImage = '';
 
   if (site.backgroundImageUrl) {
     document.body.classList.add('has-bg');
-document.body.style.backgroundImage = `url("${site.backgroundImageUrl.replace(/"/g, "%27")}")`;
-  }
+document.body.style.backgroundImage = `url("${site.backgroundImageUrl.replace(/"/g, "%27")}")`;  }
 }
 
 function showLibrary() {
